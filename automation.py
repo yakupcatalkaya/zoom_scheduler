bb1b1e75bb1e6c66954105190aa579ae1ee8acd0
Yakup Çatalkaya <31369965+yakupcatalkaya@users.noreply.github.com>
2023-04-19 18:08:51 +0300

    Update automation.py

--- automation.py
+++ automation.py
@@ -1,16 +1,18 @@
 import subprocess
 import argparse
 import os
+from google.cloud import storage
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--file", default="herringbone-gear-large.stl")
     parser.add_argument("-m", "--machine", default="fdmprinter.def.json")
     parser.add_argument("-e", "--extruder", default="prusa_i3.def.json")
+    parser.add_argument("-o", "--folder", default="")
     args = parser.parse_args()
     return args
 
-def call_cmd(printer_json, extruder_json, stl_file, number = 0):
+def call_cmd(printer_json, extruder_json, stl_file, output_folder, number = 0):
   while os.path.isfile("output_" + str(number)): number += 1
   else:output_file = "output_" + str(number)
   program_path = "/CuraEngine/build/CuraEngine"
@@ -18,11 +20,19 @@ def call_cmd(printer_json, extruder_json, stl_file, number = 0):
   command = " ".join(command)
   proc = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
   out, err = proc.communicate()
-  return [out, out.decode(), output_file]
+  bucket_name = 'cura_files'
+  storage_client = storage.Client()
+  bucket = storage_client.get_bucket(bucket_name)
+  filee = output_folder+"output.gcode"
+  blob = bucket.blob(filee)
+  with open(output_file,"rb") as myfile:
+    blob.upload_from_string(str(myfile.read().decode("latin")))
+  outfile = "https://storage.cloud.google.com/" + bucket_name + filee
+  return [out, out.decode(), outfile]
 
 
 args = get_args()
-out, decoded_out, output_file = call_cmd(args.machine, args.extruder, args.file)
+out, decoded_out, output_file = call_cmd(args.machine, args.extruder, args.file,args.folder)
 cont = open(output_file).read()
 for line in cont.split("\n")[::-1]:
   if "TIME_ELAPSED" in line:break
